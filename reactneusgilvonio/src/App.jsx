7ffe634f9d39c09570451bc1de224876a434ec1f
neusgo <neusmarianelgo@gmail.com>
2024-06-06 19:15:52 -0400

    Added the new component

---
+++ reactneusgilvonio/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import NewComponent from './NewComponent';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Welcome to React Vite Project</h1>
+        <NewComponent />
+      </div>
+    </>
+  )
+}
+
+export default App
